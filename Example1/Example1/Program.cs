473ac3adece9dde1bae0ef88289268cf72b5f1b8
Austin Vickers <adv0003@uah.edu>
2019-06-13 15:15:11 -0500

    Hello world program

--- Example1/Example1/Program.cs
+++ Example1/Example1/Program.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
 
 namespace Example1
 {
     class Program
     {
+        [Export]
+        string msg = "Hello MEF";
+
+        [Import]
+        string message;
+
+        private void Compose()
+        {
+            AssemblyCatalog catalog = new AssemblyCatalog(typeof(Program).Assembly);
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+
         static void Main(string[] args)
         {
+            var program = new Program();
+            program.Compose();
 
-
+            Console.WriteLine(program.message);
+            Console.ReadLine();
         }
     }
 }
